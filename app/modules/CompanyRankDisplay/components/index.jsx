dace92b5740eb6d47f96dc23e3186ed7c528f425
prasanna1211 <prasanna.krystal@gmail.com>
2017-09-11 16:17:34 +0530

    Added root company Rank display

---
+++ app/modules/CompanyRankDisplay/components/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CompanyRankDisplay = (props) => (
+  <div>
+    Company Rank Display
+  </div>
+);
+
+export default CompanyRankDisplay;
---
+++ app/modules/CompanyRankDisplay/index.js
@@ -0,0 +1,12 @@
+import { connect } from 'react-redux';
+import CompanyRankDisplay from '../components/index.jsx';
+
+const mapStateToProps = state => ({
+
+});
+
+const mapDispatchToProps = dispatch => ({
+
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CompanyRankDisplay);
