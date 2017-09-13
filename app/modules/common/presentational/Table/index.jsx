6aea9dcc79b2c1c070d2665ee05ab46129941491
prasanna1211 <prasanna.krystal@gmail.com>
2017-09-13 21:44:03 +0530

    JSX for table added

--- app/modules/common/presentational/Table/index.jsx
+++ app/modules/common/presentational/Table/index.jsx
@@ -2,23 +2,25 @@ import React from 'react';
 
 import map from 'lodash/map';
 
+import styles from './styles.scss';
+
 const Table = ({ data }) => {
   const renderRow = (row) => {
     return (
-      <td key={Math.random()}>
-        {row[2]}
+      <td className="cell-value" key={Math.random()}>
+        {row[2] != -1 ? row[2] : 'N/A'}
       </td>
     )
   }
 
   return (
-    <table>
+    <table className={styles['table']}>
       <tbody>
         {
-          map(data, (value, key) => {
+          map(data, (value) => {
             return (
               <tr>
-                <td>{key}</td>
+                <td>{value[0][0]}</td>
                 <td>{map(value, eachDateData => renderRow(eachDateData))}</td>
               </tr>
             )
