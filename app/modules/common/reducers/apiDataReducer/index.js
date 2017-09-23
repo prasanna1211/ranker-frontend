e133612db28b91e174ba4487e1d6fc48b0ea0232
prasanna1211 <prasanna.krystal@gmail.com>
2017-09-23 16:18:34 +0530

    Added fetch status changes

--- app/modules/common/reducers/apiDataReducer/index.js
+++ app/modules/common/reducers/apiDataReducer/index.js
@@ -6,25 +6,17 @@ import reduce from 'lodash/reduce';
 import companyRankDataReducer from '../../../CompanyRankDisplay/reducers/companyDataReducer';
 import createReducer from '../../../../helpers/createReducer';
 
-const commonDataInitialState = Map({
-  isFetched: false,
+const commonDataInitialState = fromJS({
+  domainData: {}
 });
-
-const onHtInitialFetch = (state, { payload }) => {
-  let newState = state;
-  newState = newState.setIn(['isFetched'], false);
-  return newState;
-};
 
 const onHtInitialFetchSuccess = (state, { payload }) => {
   let newState = state;
   newState = newState.setIn(['domainData'], fromJS(payload.allDomains));
-  newState = newState.setIn(['isFetched'], true);
   return newState;
 };
 
 const commonDataReducer = createReducer(commonDataInitialState, {
-  COMMON_INITIAL_FETCH: onHtInitialFetch,
   COMMON_INITIAL_FETCH_SUCCESS: onHtInitialFetchSuccess,
 });
 
